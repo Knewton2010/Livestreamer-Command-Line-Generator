87d1c3bd79b17076593aa319478445c4ec5fb878
BenKettlewell <ben.kettlewell@gmail.com>
2015-07-17 03:50:17 -0700

    More comments and minor refactoring

--- Main.py
+++ Main.py
@@ -4,6 +4,9 @@ import re
 
 
 def swapOutDashesForUnderscores(modify):
+    ''' Improve legibility by substituting all - for _
+    Given: food-wars-shokugeki-no-soma/episode-6-the-meat-invader-678171
+    '''
     dashLetter = re.compile('-')
     return dashLetter.sub("_", modify)
         
@@ -12,12 +15,19 @@ def findWordGroups (modify):
     return wordSets.findall(modify)
         
 def findEpisodeNumber (modify):
+    ''' Given a URL extract the episode number and return it in 2-digit form
+    Given: food-wars-shokugeki-no-soma/episode-6-the-meat-invader-678171
+    Return: 09
+    '''
     numberSet = re.compile('[0-9]+')
     episodeNumber = fixEpisodeNumber (numberSet.findall(modify)[0])
     return episodeNumber
 
-# to preserve order 9 > 09. 10 stays 10
 def fixEpisodeNumber (modify):
+    ''' Change single digit episodeNumbers to 2-digit eipsode numbers
+    Given:   9
+    Return: 09
+    '''
     if len(modify) < 2:
         return "0" + modify 
     return modify
@@ -53,9 +63,15 @@ def generateLivestreamerCommand (parsedList, index):
 
     return "livestreamer -o " + show + "_" + number + title + " " + url + " best; " 
 
+def generateDebugURLs ():
+    ''' Return a list of 3 sample URLS
+    '''
+    return ['http://www.crunchyroll.com/engaged-to-the-unidentified/episode-1-its-important-to-start-off-on-the-right-foot-648831',
+    'http://www.crunchyroll.com/baby-steps/episode-14-moonlight-and-the-sound-of-waves-678323',
+    'http://www.crunchyroll.com/food-wars-shokugeki-no-soma/episode-11-the-magician-from-the-east-678181']
 
 def getURLsFromUser():
-    ''' 
+    ''' Accept and store raw URLS provided by the user then return all as a list
     '''
     prompt = """Please provide Crunchyroll URLS. 
     You may paste each url one at a time hitting enter after each,
@@ -67,13 +83,19 @@ def getURLsFromUser():
     listOfURLs = [] # Array of URLs to be filled with user provided URLS 
     while True:
         userInput = raw_input()
-        if (userInput == "done" or userInput == "Done" or userInput == ""):
+        if (userInput == "debug" or userInput == "Debug"): # DEBUG option for convenient testing
+            listOfURLs.extend(generateDebugURLs())
             break
-        elif ("-season" in userInput or "-s" in userInput or "--season" in userInput):
+        elif (userInput == "done" or userInput == "Done" or userInput == ""):  # Finished input
+            break
+        elif ("www.crunchyroll.com" not in userInput): # Initial validation that it is a Crunchyroll URL
+            raise TypeError ("Input must be a crunchyroll url")
+            break
+        elif ("--season" in userInput):  # Season support
             '''Determine what season they want the next set of urls or stop season mode etc.
             '''
             raise NotImplementedError
-        else:
+        else:                                               # Add URL to the List
             listOfURLs.append(userInput)
     return listOfURLs
 
@@ -89,18 +111,18 @@ def parseOneURL(fullURL):
     Return: Food_Wars_Shokugeki_No_Soma_06_The_Meat_Invader
     '''
 
-    o = urlparse(fullURL)
-    urlPath = o.path
+    URLParsedObject = urlparse(fullURL)
+    URLPath = URLParsedObject.path
     '''urlparse returns an object seperated into its parts. For this we only care about the .path part
     Given: http://www.crunchyroll.com/food-wars-shokugeki-no-soma/episode-6-the-meat-invader-678171
     The .path will be 'food-wars-shokugeki-no-soma/episode-6-the-meat-invader-678171'
     '''
-
-    urlPath = swapOutDashesForUnderscores(urlPath)
-    wordGroups = findWordGroups(urlPath)
+    # start food-wars-shokugeki-no-soma/episode-6-the-meat-invader-678171
+    URLPath = swapOutDashesForUnderscores(URLPath) #food_wars_shokugeki_no_soma/episode_6_the_meat_invader_678171
+    wordGroups = findWordGroups(URLPath)    # [food_wars_shokugeki_no_soma, eipsode, the_meat_invader]
     ShowName = upperAfterUnderscore(wordGroups[0])
     EpisodeTitle = upperAfterUnderscore(wordGroups[2])
-    EpisodeNumber = findEpisodeNumber (urlPath)
+    EpisodeNumber = findEpisodeNumber (URLPath)
     return [ShowName, EpisodeTitle, EpisodeNumber, fullURL]
 
 def parseURLs (listOfURLs):  
